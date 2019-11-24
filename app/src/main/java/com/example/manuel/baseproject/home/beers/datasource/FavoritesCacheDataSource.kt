b24b019be4c453b1246002cf64658061e6f47656
Manuel Mato <manuel@MacBook-Air-de-Manuel.local>
2019-11-24 12:49:45 +0100

    Create two methods to kwnoe if the beer is remvoed or added

--- app/src/main/java/com/example/manuel/baseproject/home/beers/datasource/FavoritesCacheDataSource.kt
+++ app/src/main/java/com/example/manuel/baseproject/home/beers/datasource/FavoritesCacheDataSource.kt
@@ -9,15 +9,7 @@ import java.io.File
 
 class FavoritesCacheDataSource(private val gson: Gson, private val favoritesBeersFile: File) {
 
-    fun getBeers(): List<BeerCacheModel>? {
-        val json: String = favoritesBeersFile.readText(Charsets.UTF_8)
-
-        return deserializeJsonToObject(json)
-    }
-
     fun saveBeer(beerCacheModel: BeerCacheModel): Boolean {
-        var isBeerSaved = false
-
         if (favoritesBeersFile.isFile) {
             val json: String = favoritesBeersFile.readText(Charsets.UTF_8)
             val favoriteBeers = deserializeJsonToObject(json)
@@ -28,19 +20,22 @@ class FavoritesCacheDataSource(private val gson: Gson, private val favoritesBeer
             favoritesBeersFile.writeText(serializeObjectToJSON(listOf(beerCacheModel)))
         }
 
+        return isBeerSaved(beerCacheModel.id)
+    }
+
+    private fun isBeerSaved(beerId: Int): Boolean {
+        var isBeerSaved = false
+
         getBeers()?.apply {
-            isBeerSaved = any { savedBeer -> savedBeer.id == beerCacheModel.id }
+            isBeerSaved = any { savedBeer -> savedBeer.id == beerId }
         }
 
-        if (isBeerSaved) Log.i("test", "Beer saved id = ${beerCacheModel.id}")
+        if (isBeerSaved) Log.i("test", "Beer saved id = $beerId")
 
         return isBeerSaved
     }
 
     fun removeBeer(beerCacheModel: BeerCacheModel): Boolean {
-        // Para borrar, recupero toda la lista a objetos
-        var isBeerRemoved = false
-
         if (favoritesBeersFile.isFile) {
             val json: String = favoritesBeersFile.readText(Charsets.UTF_8)
             val mutableFavoritesBeers = deserializeJsonToObject(json).toMutableList()
@@ -48,22 +43,33 @@ class FavoritesCacheDataSource(private val gson: Gson, private val favoritesBeer
             mutableFavoritesBeers.let {
                 it.remove(beerCacheModel)
                 favoritesBeersFile.writeText(serializeObjectToJSON(mutableFavoritesBeers.toList()))
-
-
-                // Para saber si he borrado, tengo que leer y comprobar si existe en la lista
-                getBeers()?.apply {
-                    isBeerRemoved = any { savedBeer -> savedBeer.id == beerCacheModel.id }
-                }
             }
         }
 
-        if (isBeerRemoved) Log.i("test", "Se borra beer id = ${beerCacheModel.id}")
+
+        return isBeerRemoved(beerCacheModel.id)
+    }
+
+    private fun isBeerRemoved(beerId: Int): Boolean {
+        var isBeerRemoved = false
+
+        getBeers()?.apply {
+            isBeerRemoved = any { savedBeer -> savedBeer.id == beerId }
+        }
+
+        if (isBeerRemoved) Log.i("test", "Beer removed id = $beerId")
 
         return isBeerRemoved
     }
 
     private fun serializeObjectToJSON(beersCacheModel: List<BeerCacheModel>): String {
         return gson.toJson(beersCacheModel)
+    }
+
+    fun getBeers(): List<BeerCacheModel>? {
+        val json: String = favoritesBeersFile.readText(Charsets.UTF_8)
+
+        return deserializeJsonToObject(json)
     }
 
     // TODO Use generics
