3a585d931c0b4ab2d89c8ce76af8a3a0c80a6940
Manuel Mato <manuel@MacBook-Air-de-Manuel.local>
2019-11-25 23:06:08 +0100

    Extract the logic to compare all beers with favorites beers

--- app/src/main/java/com/example/manuel/baseproject/home/beers/domain/usecase/GetBeersUseCase.kt
+++ app/src/main/java/com/example/manuel/baseproject/home/beers/domain/usecase/GetBeersUseCase.kt
@@ -3,6 +3,7 @@ package com.example.manuel.baseproject.home.beers.domain.usecase
 import com.example.manuel.baseproject.commons.datatype.Result
 import com.example.manuel.baseproject.commons.datatype.ResultType
 import com.example.manuel.baseproject.home.beers.domain.BeersRepository
+import com.example.manuel.baseproject.home.beers.domain.model.BeerEntity
 import com.example.manuel.baseproject.home.beers.domain.model.BeersEntity
 
 class GetBeersUseCase(private val beersRepository: BeersRepository) {
@@ -15,20 +16,10 @@ class GetBeersUseCase(private val beersRepository: BeersRepository) {
 
             if (resultType == ResultType.SUCCESS) {
                 beersEntity.data?.let {
-                    val favoritesBeer = beersRepository.getFavoriteBeers().beers
-
-                    val mutableAllBeers = it.beers.toMutableList()
-                    favoritesBeer.map { favoriteBeer ->
-                        val beerUncheckedAsFavorite = mutableAllBeers.filter { noFavoriteBeer -> noFavoriteBeer.id == favoriteBeer.id }
-
-                        if (beerUncheckedAsFavorite.isNotEmpty()) {
-                            val beerToFavorite = beerUncheckedAsFavorite[0].apply { isFavorite = true }
-                            mutableAllBeers.remove(beerUncheckedAsFavorite[0])
-                            mutableAllBeers.add(beerToFavorite)
-                        }
-                    }
-
-                    val sortedBeers = getSortedAscendingBeers(BeersEntity(mutableAllBeers.toList()))
+                    val allBeersWithFavoritesChecked =
+                            getAllBeersWithFavoritesChecked(it.beers.toMutableList()).toList()
+                    val sortedBeers =
+                            getSortedAscendingBeers(BeersEntity(allBeersWithFavoritesChecked))
                     beers = Result.success(sortedBeers)
                 }
             } else {
@@ -37,6 +28,22 @@ class GetBeersUseCase(private val beersRepository: BeersRepository) {
         }
 
         return beers
+    }
+
+    private fun getAllBeersWithFavoritesChecked(mutableAllBeers: MutableList<BeerEntity>): MutableList<BeerEntity> {
+        val favoritesBeer = beersRepository.getFavoriteBeers().beers
+
+        favoritesBeer.map { favoriteBeer ->
+            val beerUncheckedAsFavorite = mutableAllBeers.filter { noFavoriteBeer -> noFavoriteBeer.id == favoriteBeer.id }
+
+            if (beerUncheckedAsFavorite.isNotEmpty()) {
+                val beerToFavorite = beerUncheckedAsFavorite[0].apply { isFavorite = true }
+                mutableAllBeers.remove(beerUncheckedAsFavorite[0])
+                mutableAllBeers.add(beerToFavorite)
+            }
+        }
+
+        return mutableAllBeers
     }
 
     private fun getSortedAscendingBeers(beersEntity: BeersEntity): BeersEntity {
