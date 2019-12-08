47807a4f7ed6161d204e253931ea32556b4a4608
Manuel Mato <manuel@MacBook-Air-de-Manuel.local>
2019-12-08 15:45:48 +0100

    Removed animation of favorite view holder

--- app/src/main/java/com/example/manuel/baseproject/home/favorites/ui/adapterlist/FavoriteViewHolder.kt
+++ app/src/main/java/com/example/manuel/baseproject/home/favorites/ui/adapterlist/FavoriteViewHolder.kt
@@ -1,15 +1,11 @@
 package com.example.manuel.baseproject.home.favorites.ui.adapterlist
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.graphics.drawable.Drawable
 import android.view.View
-import android.view.ViewPropertyAnimator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.manuel.baseproject.R
+import com.example.manuel.baseproject.home.favorites.ui.adapterlist.model.FavoriteBeerAdapterModel
 import com.example.manuel.baseproject.view.extensions.applyBackgroundColor
 import com.example.manuel.baseproject.view.extensions.loadImage
-import com.example.manuel.baseproject.home.favorites.ui.adapterlist.model.FavoriteBeerAdapterModel
 import kotlinx.android.synthetic.main.item_list_beer.view.*
 
 class FavoriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -29,56 +25,6 @@ class FavoriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun setOnClickListener(doOnFavoriteBeerSelected: ((FavoriteBeerAdapterModel) -> Unit)?, beer: FavoriteBeerAdapterModel) {
         itemView.setOnClickListener {
             doOnFavoriteBeerSelected?.invoke(beer)
-            animateFavoriteIcon(beer.isFavorite)
-        }
-    }
-
-    private fun animateFavoriteIcon(isFavoriteBeer: Boolean) {
-        val viewPropertyAnimator = itemView.item_list_beer_favorite_button.animate()
-
-        viewPropertyAnimator
-                .scaleX(0.5f)
-                .scaleY(0.5f)
-                .setDuration(250)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        populateFavoriteIconView(isFavoriteBeer)
-                        restartFavoriteIconSize(viewPropertyAnimator)
-                    }
-
-                    override fun onAnimationStart(animation: Animator?) {
-                        super.onAnimationStart(animation)
-                        itemView.isClickable = false
-                    }
-                })
-    }
-
-    private fun restartFavoriteIconSize(viewPropertyAnimator: ViewPropertyAnimator) {
-        viewPropertyAnimator
-                .scaleX(1f)
-                .scaleY(1f)
-                .setDuration(250)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        viewPropertyAnimator.cancel()
-                        itemView.isClickable = true
-                    }
-                })
-    }
-
-    private fun populateFavoriteIconView(isFavorite: Boolean) {
-        getFavoriteIcon(isFavorite)?.let {
-            itemView.item_list_beer_favorite_button.background = it
-        }
-    }
-
-    private fun getFavoriteIcon(isFavorite: Boolean): Drawable? {
-        return if (isFavorite) {
-            itemView.context.getDrawable(R.drawable.ic_star_white_24dp)
-        } else {
-            itemView.context.getDrawable(R.drawable.ic_star_border_white_24dp)
         }
     }
 }
